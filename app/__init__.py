847a028fae2ac6f5ff0f00e52ca8028a08787ba8
kum83 <momo821123@outlook.com>
2021-08-12 09:35:29 +0300

    test

--- app/__init__.py
+++ app/__init__.py
@@ -31,8 +31,8 @@ mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'john1'  
 app.config['MYSQL_DATABASE_PASSWORD'] = 'aB22yy98'  
 app.config['MYSQL_DATABASE_DB'] = 'pffp'  
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'  
-# app.config['MYSQL_DATABASE_HOST'] = 'mysql.fpproject.dreamhosters.com'  
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'  
+app.config['MYSQL_DATABASE_HOST'] = 'mysql.fpproject.dreamhosters.com'  
 mysql.init_app(app)
 
 conn = mysql.connect()
